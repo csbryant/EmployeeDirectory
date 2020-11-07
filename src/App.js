9cf1f5f142f13129a717f01a28bb43a47e9af346
Chris Bryant <chris_bryant@mac.com>
2020-11-06 17:14:00 -0800

    Re-organized components and css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import "./App.css";
 
 import "./App.css";
 
